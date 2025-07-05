876a7614e94c6bd9c8ccec84198df1ca5559e762
jay-sharmaa <sjaysharma00@gmail.com>
2025-07-05 11:19:31 +0530

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,8 +103,6 @@ export default function AboutPage() {
     formData.append("inputfile", uploadedFile);
     formData.append("filename", "output.mp4");
 
-    // const backendUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL;
-
     try {
       const response = await fetch(`https://video-registor.onrender.com/upload`, {
         method: "POST",
